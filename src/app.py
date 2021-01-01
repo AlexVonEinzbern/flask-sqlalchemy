625c8972d6fec54659f0561cc77961284d936ef7
M. Alexander Díaz <mario.diaz@correounivalle.edu.co>
2021-01-01 11:31:01 -0500

    Cambios en los nombres de algunas variables y se agragó la librería flask-cors

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from datetime import datetime, timedelta
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:4826494629@localhost:5432/NoteApp'
@@ -9,12 +10,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-
+CORS(app)
 
 class Nota(db.Model):
     id_nota = db.Column(db.Integer, primary_key=True)
-    titulo_nota = db.Column(db.String(50), unique=True, nullable=False)
-    text_nota = db.Column(db.String(200), nullable=False)
+    titulo = db.Column(db.String(50), unique=True, nullable=False)
+    descripcion = db.Column(db.String(200), nullable=False)
     fecha_nota = db.Column(db.DateTime, nullable=False, default=datetime.utcnow()-timedelta(hours=5))
 
     def __repr__(self):
@@ -24,17 +25,17 @@ db.create_all()
 
 class NotaSchema(ma.Schema):
     class Meta:
-        fields = ('id_nota', 'titulo_nota', 'text_nota', 'fecha_nota')
+        fields = ('id_nota', 'titulo', 'descripcion', 'fecha_nota')
 
 nota_schema = NotaSchema()
 notas_schema = NotaSchema(many=True)
 
-@app.route('/notas', methods=['POST'])
+@app.route('/crearNotas', methods=['POST'])
 def create_nota():
-    titulo = request.json['titulo_nota']
-    texto = request.json['text_nota']
+    titulo = request.json['titulo']
+    descripcion = request.json['descripcion']
 
-    new_nota = Nota(titulo_nota=titulo, text_nota=texto)
+    new_nota = Nota(titulo=titulo, descripcion=descripcion)
     db.session.add(new_nota)
     db.session.commit()
     return nota_schema.jsonify(new_nota)
